df519454498f95f01c9b1811ff56c5002114d5d8
ting-prado <ethel.prado1998@gmail.com>
2022-04-04 22:31:03 +0800

    Started displaying current weather info to app

--- src/index.js
+++ src/index.js
@@ -1,36 +1,65 @@
 import { format, addDays } from 'date-fns';
 
-const getCoordinates = async (location) => {
+const searchBar = document.querySelector('#searchbar');
+const searchBtn = document.querySelector('button');
+const currentIcon = document.querySelector('.currentIcon');
+const currentLocation = document.querySelector('.location');
+const currentTemp = document.querySelector('.currentTemp');
+const currentDesc = document.querySelector('.currentDesc');
+const currentTime = document.querySelector('.currentTime');
+
+const getCoord = async (location) => {
   const response = await fetch(
     `https://api.openweathermap.org/geo/1.0/direct?q=${location}&appid=045fce2ab9cadc090005d293ba386801`,
     { mode: 'cors' }
   );
-
   const data = await response.json();
-  console.log(data);
+
   const coord = {
     lat: data[0].lat,
     lon: data[0].lon,
+    loc: `${data[0].name}, ${data[0].country}`,
   };
+
   return coord;
 };
 
-const getWeatherData = async (coord) => {
-  console.log(`Latitude: ${coord.lat}\nLongitude: ${coord.lon}`);
+const getWeather = async (coord) => {
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/onecall?lat=${coord.lat}&lon=${coord.lon}&exclude=minutely,hourly,alerts&appid=045fce2ab9cadc090005d293ba386801`,
     { mode: 'cors' }
   );
-  const data = await response.json();
-  console.log(data);
-  const date = new Date();
-  console.log(`Updated as of: ${format(date, 'h:mmbbb')}`);
-  console.log(format(addDays(date, 1), 'ccc LLL d'));
+  const complete = await response.json();
+  const data = {
+    loc: coord.loc,
+    current: complete.current,
+    daily: complete.daily,
+  };
+  return data;
 };
 
-getCoordinates('llkkkjasf')
-  .then(getWeatherData)
-  .catch((err) => {
-    // Do not change currently displayed info
-    console.log(`An error occured: ${err}`);
-  });
+const displayData = (weather) => {
+  currentLocation.textContent = weather.loc.toUpperCase();
+  currentTemp.textContent = `${weather.current.temp}°`;
+  currentDesc.textContent =
+    weather.current.weather[0].description.toUpperCase();
+  currentTime.textContent = `Updated as of: ${format(new Date(), 'h:mmbbb')}`;
+  currentIcon.src = `https://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png`;
+};
+
+const getData = (location) => {
+  getCoord(location)
+    .then(getWeather)
+    .then(displayData)
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+searchBtn.addEventListener('click', () => {
+  getData(searchBar.value);
+});
+
+window.addEventListener('load', () => {
+  getData('Gingoog City');
+});
